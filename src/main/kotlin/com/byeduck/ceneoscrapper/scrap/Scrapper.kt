4d8239a7d2b4ef917eec58bc62bb6e107aa4d81b
byeduck <mateuszbajdak@gmail.com>
2022-08-24 04:58:34 +0200

    feature: product url

--- src/main/kotlin/com/byeduck/ceneoscrapper/scrap/Scrapper.kt
+++ src/main/kotlin/com/byeduck/ceneoscrapper/scrap/Scrapper.kt
@@ -5,16 +5,21 @@ import com.byeduck.ceneoscrapper.model.ProductCategory
 import com.byeduck.ceneoscrapper.model.ProductReviewScore
 import com.byeduck.ceneoscrapper.rest.CeneoFilter
 import org.jsoup.Jsoup
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
 
 @Component
-class Scrapper {
+class Scrapper(
+    @Value("\${scrap.ceneo-base-url}") private val ceneoBaseUrl: String
+) {
 
     fun scrapCeneoPage(htmlPage: String, cacheKey: String): List<Product> =
         Jsoup.parseBodyFragment(htmlPage).body().getElementsByClass(PRODUCT_ROW_CLASS).toList().map {
             val brandName = it.attr(PRODUCT_ROW_BRAND_ATTRIBUTE_SELECTOR).sanitize().ifBlank { "N/A" }
-            val productName = it.selectFirst(PRODUCT_ROW_NAME_SELECTOR)?.selectFirst("span")?.text() ?: "N/A"
+            val productNameElement = it.selectFirst(PRODUCT_ROW_NAME_SELECTOR)?.selectFirst("a")
+            val productRelativeUrl = productNameElement?.attr("href")
+            val productName = productNameElement?.selectFirst("span")?.text() ?: "N/A"
             val productPriceSpan = it.selectFirst(PRODUCT_PRICE_SPAN_SELECTOR)
             val productScore = it.selectFirst(PRODUCT_SCORE_SPAN_SELECTOR)?.text()?.sanitize() ?: "0/0"
             val productPriceInt = productPriceSpan?.selectFirst(PRODUCT_PRICE_INTEGER_SPAN_SELECTOR)?.text() ?: "0"
@@ -22,11 +27,21 @@ class Scrapper {
             val productPrice = BigDecimal.valueOf(
                 (productPriceInt.sanitize() + productPriceDec.replace(",", ".").sanitize()).toDouble()
             )
-            return@map Product(productName, brandName, productPrice, ProductReviewScore.parse(productScore))
+            return@map Product(
+                productName,
+                brandName,
+                getProductUrl(productRelativeUrl),
+                productPrice,
+                ProductReviewScore.parse(productScore)
+            )
         }.drop(1) // first product is not correlated with query
 
     fun createCacheKey(category: ProductCategory, filter: CeneoFilter): String =
         "$category:${filter.query}:${filter.minPrice}:${filter.maxPrice}"
 
     private fun String.sanitize(): String = this.replace("\\s".toRegex(), "").replace(",", ".")
+
+    private fun getProductUrl(relativeUrl: String?): String? {
+        return if (relativeUrl == null) null else ceneoBaseUrl + relativeUrl
+    }
 }
